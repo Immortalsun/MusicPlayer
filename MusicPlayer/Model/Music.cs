0c841e4fc5f492dbd5afd124c99f8234d69d966a
Immortalsun <maashes.p@gmail.com>
2015-11-20 11:53:16 -0500

    File commit

---
+++ MusicPlayer/Model/Music.cs
@@ -0,0 +1,54 @@
+﻿namespace MusicPlayer.Model
+{
+    public class Music
+    {
+        #region Fields
+        private string _filePath, _name, _artist, _album;
+        private bool _isPlaying;
+        #endregion
+
+        #region Properties
+        public string FilePath
+        {
+            get { return _filePath; }
+            set { _filePath = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Artist
+        {
+            get { return _artist; }
+            set { _artist = value; }
+        }
+
+        public string Album
+        {
+            get { return _album; }
+            set { _album = value; }
+        }
+
+        public bool IsPlaying
+        {
+            get { return _isPlaying; }
+            set { _isPlaying = value; }
+        }
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Events
+
+        #endregion 
+    }
+}
