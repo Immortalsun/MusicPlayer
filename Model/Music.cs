45aece856c6af8f5beb43d712996b2f395595420
Immortalsun <maashes.p@gmail.com>
2015-12-06 17:16:03 -0500

    File commit

---
+++ Model/Music.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MusicPlayer.Model
+{
+    public class Music
+    {
+        #region Fields
+        private string _filePath, _name, _artist, _album;
+        #endregion
+
+        #region Properties
+        public string FilePath
+        {
+            get { return _filePath; }
+            set { _filePath = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Artist
+        {
+            get { return _artist; }
+            set { _artist = value; }
+        }
+
+        public string Album
+        {
+            get { return _album; }
+            set { _album = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public Music(string filePath)
+        {
+            FilePath = filePath;
+            SetupSongMetadata();
+        }
+        #endregion
+
+        #region Methods
+        
+
+        public void SetupSongMetadata()
+        {
+            TagLib.File musicFile = TagLib.File.Create(FilePath);
+            Name = musicFile.Tag.Title;
+            GenerateArtistList(musicFile.Tag.Performers);
+            Album = musicFile.Tag.Album;
+        }
+
+        public void GenerateArtistList(string[] performers)
+        {
+            if(!performers.Any())
+                return;
+
+            if (performers.Count() == 1)
+            {
+               Artist = performers[0];
+            }
+            else 
+            {
+                for (int i = 0; i < performers.Length; i++)
+                {
+                    Artist += performers[i];
+
+                    if (i < performers.Length - 1)
+                    {
+                        Artist += ", ";
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Events
+
+        #endregion 
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using MusicPlayer.ViewModel;
+
+namespace MusicPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MusicPlayerViewModel _viewModel;
+
+        public MainWindow()
+        {
+            _viewModel = new MusicPlayerViewModel();
+            InitializeComponent();
+            DataContext = _viewModel;
+        }
+    }
+}
